091798b1bd6b588951f031d6fde723a6c1cdab52
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-08-26 01:31:00 -0700

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, r = 0, 0
+        count = {}
+        max_window = 0
+
+        while r < len(s):
+            count[s[r]] = count.get(s[r], 0) + 1
+
+            max_freq = max(count.values())
+            replacemets = (r - l + 1) - max_freq
+
+            while replacemets > k:
+                count[s[l]] -= 1
+                l += 1
+                replacemets = (r - l + 1) - max_freq
+                max_freq = max(count.values())
+
+            max_window = max(max_window, r - l + 1)
+            r += 1
+
+        return max_window
