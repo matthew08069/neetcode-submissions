caab151cbe137ee83750aaf87e5addac37c01e04
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-08-30 21:38:32 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # (n-1) + (n-2) = ways to climb nth stairs
+        s1 = 1
+        s2 = 2
+
+        for _ in range(n - 2):
+            temp = s1
+            s1 = s2
+            s2 = temp + s2
+
+        return s2 if n > 1 else s1
