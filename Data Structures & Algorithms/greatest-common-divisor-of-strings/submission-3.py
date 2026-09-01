153213f03ea912c18b427b915876a91ab2dd2315
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-08-31 22:53:20 -0700

    Add: greatest-common-divisor-of-strings - submission-3

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        s1 = len(str1)
+        s2 = len(str2)
+        remin = 1
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        while remin != 0:
+            remin = s1 % s2
+            s1 = s2
+            s2 = remin
+
+        return str2[0:s1]
