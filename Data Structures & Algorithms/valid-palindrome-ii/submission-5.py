e1df32b6cc8de7a1c7df8086d99bc44539bcb058
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-09-01 22:40:55 -0700

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def palindrome(s):
+            l, r = 0, len(s) - 1
+
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        l, r = 0, len(s) - 1
+
+        while l <= r:
+            if s[l] != s[r]:
+                # Try removing s[l]
+                if palindrome(s[0:l] + s[l + 1 :]):
+                    return True
+                else:
+                    # Try removing s[r]
+                    return palindrome(s[0:r] + s[r + 1 :])
+            l += 1
+            r -= 1
+
+        return True
