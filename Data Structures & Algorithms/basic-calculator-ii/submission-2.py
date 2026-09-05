248920fe277f9e2a0090123471a507a942333456
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-09-05 01:45:14 -0700

    Add: basic-calculator-ii - submission-2

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        operator = ["+", "-", "*", "/"]
+        res = []
+        num = 0
+        op = "+"
+
+        for i in range(len(s)):
+            char = s[i]
+
+            if char.isdigit():
+                num = num * 10 + int(char)
+
+            if char in operator or i == len(s) - 1:
+                if op == "+":
+                    res.append(num)
+                elif op == "-":
+                    res.append(-num)
+                elif op == "*":
+                    num = res.pop() * num
+                    res.append(num)
+                elif op == "/":
+                    num = int(res.pop() / num)
+                    res.append(num)
+
+                if char != " ":
+                    op = char
+                num = 0
+
+        return sum(res)
