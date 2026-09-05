f61d00c58875c57f0fec670ce180ad8e5539bb07
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-09-04 23:56:03 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dfs(i):
+            # Base case
+            # If house in memo, return the max sum from that house
+            if i in memo:
+                return memo[i]
+            # If there's no more house can be robbed, return that house
+            if i >= len(nums):
+                return 0
+            rob = nums[i] + dfs(i + 2)
+            skip = dfs(i + 1)
+
+            memo[i] = max(rob, skip)
+
+            return memo[i]
+
+        return dfs(0)
