988bca8a70771bb906056fa406be8dad3f9c70f4
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-09-06 15:57:15 -0700

    Add: find-the-town-judge - submission-4

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if n == 1 and not trust:
+            return 1
+        # A dictionary with {bi:ai}
+        table = {}
+        trusters = set()
+
+        for i in trust:
+            if i[1] in table:
+                table[i[1]].append(i[0])
+                trusters.add(i[0])
+                continue
+            table[i[1]] = [i[0]]
+            trusters.add(i[0])
+
+        for canidate in table:
+            if len(table[canidate]) == n - 1:
+                if not canidate in trusters:
+                    return canidate
+
+        return -1
+            
+            
