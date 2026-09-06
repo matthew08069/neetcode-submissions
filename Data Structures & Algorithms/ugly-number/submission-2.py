b7c897c7c6e23be879d0e9142615151890bf742d
Matthew Chan <62037940+matthew08069@users.noreply.github.com>
2026-09-06 14:45:28 -0700

    Add: ugly-number - submission-2

---
+++ Data Structures & Algorithms/ugly-number/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        res = True
+        
+        while res:
+            if n == 1:
+                res = True
+                break
+            if n % 2 == 0:
+                n = n//2
+            elif n % 3 == 0:
+                n = n//3
+            elif n % 5 == 0:
+                n = n//5
+            else:
+                res = False
+        return res
